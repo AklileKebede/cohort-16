9ddf436d1b14d1f4c3046eeddb24a2444b931267
Aklile Kebede <aklile123@gmail.com>
2021-02-08 10:06:36 -0500

    about to do the ReadFile

---
+++ Module-1/Assessment/Assessment/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assessment
+{
+    public class Car
+    {
+        public int YearManufactured { get; set; }
+        public string Make { get; set; }
+        public bool IsClassicCar { get; set; }
+        public int Age { get; set; }
+
+        public Car(int yearManufactured, string make, bool isClassicCar)
+        {
+            this.YearManufactured = yearManufactured;
+            this.Make = make;
+            this.IsClassicCar = isClassicCar;
+            this.Age = DateTime.Now.Year - this.YearManufactured;
+        }
+
+        public bool ECheck(int yearToCheck)
+        {
+            
+            
+            if (this.YearManufactured < 4 || this.YearManufactured > 25)
+            {
+                return false;
+            }
+            else if ( this.YearManufactured%2==0 && yearToCheck%2==0)// if Even-model year vehicles must be tested if yearToCheck is even.
+            {
+                return true;
+            }
+            else if (this.YearManufactured%2!=0 && yearToCheck%2!=0) // Odd-model year vehicles must be tested if yearToCheck is odd.
+            {
+                return true;
+            }
+            return true;
+
+        }
+
+        public override string ToString()
+        {
+            return $"CAR - {this.YearManufactured} - {this.Make}";
+        }
+
+
+
+    }
+}
--- Module-1/Assessment/Assessment/Program.cs
+++ Module-1/Assessment/Assessment/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Assessment
 {
@@ -7,7 +8,30 @@ namespace Assessment
         static void Main(string[] args)
         {
             // TODO: Create instances of your object here and call methods.
+            Car Cobalt = new Car(2008, "Shevy", false);
+            Car Forester = new Car(1999, "Subaru", true);
 
+            // and use the object(s) to test your methods.
+            // 1. ECheck(int yearToCheck)
+            // 2. ToString()
+
+            Console.WriteLine("Test Method ECheck");
+            Console.WriteLine(Cobalt.ECheck(2023)); // False
+            Console.WriteLine(Forester.ToString()); // CAR - 1999 - Subaru
+
+            string file = "CarInput.csv,";
+
+           using (StreamReader sr = new StreamReader(file))// not reading.
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+
+                    string[] carFields = line.Split(",");
+                    int year = int.Parse(carFields[0]);
+                    Console.WriteLine(year);
+                }
+            }
         }
     }
 }
