4a2df46a0e75060152bcef5854c60abb6b59d617
Aklile Kebede <aklile123@gmail.com>
2021-03-18 20:13:10 -0400

    08_DOM: Completed

--- Module-3/08_DOM/student-exercise/shopping-list/js/shopping-list.js
+++ Module-3/08_DOM/student-exercise/shopping-list/js/shopping-list.js
@@ -1,27 +1,50 @@
 // add pageTitle
+const pageTitle = 'My Shopping List';
 
 // add groceries
+const groceries = ['SoFresh Produce', 'Baking Goods', 'Condiments', 'Grains', 'Freezer Items', 'Bread', 'Protein', 'Dairy', 'Dried Goods', 'Snacks'];
+
 
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function setPageTitle() {}
+ function setPageTitle() {
+  // Reference DOM location
+  let titlePlace = document.getElementById('title');
+  // Set property
+   titlePlace.innerText = pageTitle;
+   
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
-
+function displayGroceries() {
+  const ul = document.getElementById('groceries');
+  groceries.forEach((item) => {
+    const li = document.createElement('li');
+    li.innerText = item;
+    ul.appendChild(li);    
+  });
+  // document.getElementById('shopping-list').appendChild(ul);
+}
+ 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
-function markCompleted() {}
+function markCompleted() {
+  const lis = document.getElementById('groceries').getElementsByTagName('li');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].classList.add('completed');
+  };
+}
 
 setPageTitle();
 
 displayGroceries();
+
 
 // Don't worry too much about what is going on here, we will cover this when we discuss events.
 document.addEventListener('DOMContentLoaded', () => {
--- Module-3/09_Event_Handling/student-lecture/product-reviews/js/app.js
+++ Module-3/09_Event_Handling/student-lecture/product-reviews/js/app.js
@@ -60,6 +60,7 @@ function setPageDescription() {
  */
 function displayReviews() {
   let container = document.getElementById('reviews');
+  container.innerHTML = '';
   reviews.forEach((rev) => {
     // Step 1: Create the element
     let reviewDiv = document.createElement('div');
@@ -106,7 +107,7 @@ function addRating(parent, numberOfStars) {
   // Set properties
   ratingDiv.classList.add('rating');
 
-  for (let i = 1; i <= numberOfStars; i++){
+  for (let i = 1; i <= numberOfStars; i++) {
     // Create an img
     let img = document.createElement('img');
     img.classList.add('ratingStar');
@@ -149,36 +150,74 @@ function addReview(parent, review) {
 //  We are going to hook up more event handlers, so let's clean it up by creating an initializePage function 
 //  to do all our setup stuff.
 
-// set the product reviews page title
-setPageTitle();
-// set the product reviews page description
-setPageDescription();
-// display all of the product reviews on our page
-displayReviews();
-
-// TODO 02: Allow the user to update the Description
-// TODO 02a: Handle 'click' on the description, and show the edit box, 
-//            while hiding the p with the description in it
-// TODO 02b: Handle the keyup event to determine when the user is done editting. If the user pressed Enter, 
-//            save the desc. If they pressed Esc, quit without saving. For all other keys, let the event bubble up.
-// TODO 02c: Also handle the blur event on the textbox, which does the same as Esc (quit without saving).
-
-// TODO 03: Allow the user to add a new Review
-// TODO 03a: Handle the click event of the Add Review button. Show the form (call showHideForm())
-// TODO 03c: Handle the click event of the Save Review button, and call saveReview().
+document.addEventListener('DOMContentLoaded', initializePage); //"hi dom, when you are done loading let me know"
 
 /**
- * Swap out the description text for a text box to allow the user to edit the description.
- *
- * @param {Event} event the event object
+ * This function is called once the DOM has been loaded,
+ * and it sets up all our event handlers, and display data on
+ * the page.
  */
- function enterDescriptionEdit(desc) {
-  const textBox = desc.nextElementSibling;
-  textBox.value = description;
-  textBox.classList.remove('d-none');
-  desc.classList.add('d-none');
-  textBox.focus();
+function initializePage() {
+  // set the product reviews page title
+  setPageTitle();
+  // set the product reviews page description
+  setPageDescription();
+  // display all of the product reviews on our page
+  displayReviews();
+  
+  // Get the description "p" element
+  let descP = document.querySelector('p.description');
+  // TODO 02a: Handle 'click' on the description, and show the edit box, 
+  //            while hiding the p with the description in it
+  descP.addEventListener('click', (ev) => {
+    // alert('Hey'); // used it to make sure that the listener is working
+    // Show input, hide p
+    enterDescriptionEdit(ev.target);
+  });
+  // TODO 02b: Handle the keyup event to determine when the user is done editting. If the user pressed Enter, 
+  //            save the desc. If they pressed Esc, quit without saving. For all other keys, let the event bubble up.
+  document.getElementById('inputDesc').addEventListener('keyup', (ev) => {
+    if (ev.key === 'Enter') {
+      exitDescriptionEdit(ev.target, true);
+    } else if (ev.key === 'Escape') {
+      exitDescriptionEdit(ev.target, false);
+    }
+  });
+  // TODO 02: Allow the user to update the Description
+
+  // TODO 02c: Also handle the blur event on the textbox, which does the same as 
+  // Esc(quit without saving).
+
+  document.getElementById('inputDesc').addEventListener('blur', (ev) => {
+    exitDescriptionEdit(ev.target, false);
+  });
+
+  // TODO 03: Allow the user to add a new Review
+  // TODO 03a: Handle the click event of the Add Review button. Show the form (call showHideForm())
+  document.getElementById('btnToggleForm').addEventListener('click', showHideForm);
+
+
+  // TODO 03c: Handle the click event of the Save Review button, and call saveReview().
+  document.getElementById('btnSaveReview').addEventListener('click', (ev) => {
+    ev.preventDefault(); // every even has a default, this enables it!
+    saveReview();
+  });
+  //document.getElementById('btnSaveReview').addEventListener('click', saveReview()); doesn't work
+
+  /**
+   * Swap out the description text for a text box to allow the user to edit the description.
+   *
+   * @param {Event} event the event object
+   */
+  function enterDescriptionEdit(desc) {
+    const textBox = desc.nextElementSibling;
+    textBox.value = description;
+    textBox.classList.remove('d-none');
+    desc.classList.add('d-none');
+    textBox.focus();
+  }
 }
+
 
 /**
  * Swap back from the Textbox to the text element. if "save" is true, change the value of the 
@@ -212,7 +251,20 @@ function showHideForm() {
       * Hide the form
       * Set the button text back to 'Add Review'
   */
+  let form = document.querySelector('form');
+  let addButton = document.getElementById('btnToggleForm');
 
+  if (form.classList.contains('d-none')) {
+    form.classList.remove('d-none');
+    addButton.innerText = "Hide Form";
+    document.getElementById('name').focus();
+  form.classList.remove('d-none');
+  } else {
+    resetFormValues();
+    form.classList.add('d-none');
+    addButton.innerText = "Add Review"
+  };
+  
 }
 
 /**
@@ -241,5 +293,23 @@ function saveReview() {
   * Call displayReviews, passing in the new review
   * Hide the form (call showHideForm)
   */
+  let nameInput = document.getElementById('name');
+  let titleInput = document.getElementById('title');
+  let ratingSelect = document.getElementById('rating');
+  let reviewTextArea = document.getElementById('review');
+  // Create the new review object
+  let newReview = {
+    reviewer: nameInput.value,
+    title: titleInput.value,
+    rating: ratingSelect.value,
+    review: reviewTextArea.value
+  };
+  // Add it to the array of reviews
+  reviews.unshift(newReview);
 
+  // Now display it in the html
+  displayReviews();
+
+  //Hide the form
+  showHideForm();
 }
